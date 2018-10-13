a4c0d94989d0a1d12e29bbef8e10c7f65d413b8e
Akira <akirbaes@ulb.ac.be>
2018-10-13 15:48:51 +0200

    Merged the two versions with params
    A batch file can be used to drag-drop images easily

--- rgb_converter.py
+++ rgb_converter.py
@@ -1,11 +1,35 @@
 from __future__ import print_function
 from PIL import Image
 from math import ceil
-"""Usage: python rgb_converter.py somesnake.gif
+from statistics import mean
+import sys
+"""Usage: python rgb_converter.py somesnake.gif [vscale] [r] [g] [b]
 Returns: the image converted into subpixels horizontally (each three pixel goes into R, G, B)
-For PNGs, a gif of the three offsets and three images of the offsets
-For GIFs, three gifs of the three offsets"""
+If "vscale" is mentionned, scale vertically (average greyscale)
+If r, g or b are mentionned, output with the given pixel as leftmost
+Accepts gifs and pngs."""
 	
+filename = "ghost.gif"
+try:
+    filename=sys.argv[1]
+except:
+    print("No argument parameter given.\nUsing default filename:",filename)
+
+sysargs = sys.argv[2:]
+vscale = 0
+
+vscale = "vscale" in sysargs
+
+print("vscale:",("No","Yes")[vscale])
+	
+offsets = ("r" in sysargs, "g" in sysargs, "b" in sysargs)
+if(offsets == (0,0,0)):
+	offsets = (0,0,1)
+	
+print("Offset sets:","r (0) "*offsets[0]+"g (1) "*offsets[1]+"b (2) "*offsets[2])
+
+x_offsets = (index for index, value in enumerate(offsets) if value==1)
+
 def save_gif_sequence(images,outname, duration = 1000/20):
 	#https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#gif
 	first = images[0].convert("P")
@@ -21,68 +45,17 @@ def load_gif_animation(gifname):
 			image.seek(image.tell() + 1)
 	except EOFError:
 		return
-		
-def main_for_png(filename):
-	original = Image.open(filename)
-	results = []
-	for i in range(3):
-		res = turn_image(original,i)
-		res.save(filename+"_"+"rgb"[i]+".png")
-		results.append(res)
-	save_gif_sequence(results+[results[1]],filename+"_rgb.gif",1000/4)
 
-def main_for_gif(filename):
-	results_r = []
-	results_g = []
-	results_b = []
-	for image in load_gif_animation(filename):
-		results_r.append(turn_image(image,0))
-		results_g.append(turn_image(image,1))
-		results_b.append(turn_image(image,2))
-	save_gif_sequence(results_r,filename+"_r.gif")
-	save_gif_sequence(results_g,filename+"_g.gif")
-	save_gif_sequence(results_b,filename+"_b.gif")
-	#save_gif_sequence(results_r+results_g+results_b+results_g,"snake_rgbmove.gif")
-	
-def main():
-	from sys import argv
-	if(len(argv)==1):
-		argv.append("somesnake.gif")
-	if(argv[1][-4:]==".gif"):
-		main_for_gif(argv[1])
-	else:
-		main_for_png(argv[1])
-	
-def old_main():
-	original = Image.open("skull.png")
-	print(original.format, original.size, original.mode)
-	results = []
-	for i in range(3):
-		res = turn_image(original,i)
-		#res.show()
-		res.save("_skull_"+"rgb"[i]+".png")
-		results.append(res)
-	save_gif_sequence(results+[results[1]],"_skull_rgb.gif",1000/4)
-	
-	results_r = []
-	results_g = []
-	results_b = []
-	for image in load_gif_animation("somesnake.gif"):
-		results_r.append(turn_image(image,0))
-		results_g.append(turn_image(image,1))
-		results_b.append(turn_image(image,2))
-	save_gif_sequence(results_r,"snake_r.gif")
-	save_gif_sequence(results_r+results_g+results_b+results_g,"snake_rgbmove.gif")
-	
-	
-	
-def turn_image(original, offset=0):
+def turn_image(original, offset=0, vscale=False):
 	if(offset not in (0,1,2)):
 		print("Offset must be 0, 1 or 2. Risk of error.")
-	original = original.convert("L")
+	yscale = (1,3)[vscale]
+	
+	original = original.convert("L") #Convert into grayscale
 	old_width = original.size[0]
-	new_width = int(ceil((old_width+2)/3.0))
-	new_height = original.size[1]
+	new_width = int(ceil(float(old_width+offset)/3)) #One third, but offset possible
+	old_height = original.size[1]
+	new_height = int(ceil(float(old_height)/yscale)) #One third or not
 
 	output = Image.new("RGB", (new_width,new_height))
 	for y in range(new_height):
@@ -93,15 +66,38 @@ def turn_image(original, offset=0):
 				if(old_x<offset):
 					value = 0
 				else:
-					value = original.getpixel((old_x-offset,y))
+					values = []
+				
+					for old_y in range(y*yscale,min(y*yscale+yscale,old_height)):
+						values.append(original.getpixel((old_x-offset,old_y)))
+					value = mean(values) #Average of the three vertical pixels
+					
 				#print(value)
 				rgb.append(int(value))
 			#print(rgb)
 			if(len(rgb)!=3):
-				rgb.extend([0]*(3-len(rgb)))
+				print("Border lost:",rgb)
+				rgb+=[0]*(3-len(rgb))
 			output.putpixel((x,y),tuple(rgb))
 	return output
 		
 		
 if __name__ == "__main__":
-	main()
+	for offset in x_offsets:
+		#This way long gifs are read three times: is it a bad idea?
+		result = []
+		if(filename[-4:]==".gif"):
+			images = load_gif_animation(filename)
+		else:
+			images = (Image.open(filename),)
+		for image in images:
+			result.append(turn_image(image,offset,vscale))
+			
+		name = filename[:-4] + "_" + "RGB"[offset]+("_tall","_small")[vscale]
+		
+		if(len(result)>1):
+			save_gif_sequence(result,name+".gif")
+			print("Saved",name+".gif")
+		else:
+			result[0].save(name+".png")
+			print("Saved",name+".png")
