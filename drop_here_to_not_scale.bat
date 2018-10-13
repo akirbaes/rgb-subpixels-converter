18970abe37742f9083944cc48b81399a27e9e119
Akira <akirbaes@ulb.ac.be>
2018-10-13 17:29:15 +0200

    Less files generated by .bat

--- drop_here_to_not_scale.bat
+++ drop_here_to_not_scale.bat
@@ -1,6 +1,6 @@
 CD %~dp0
 FOR %%A IN (%*) DO (
 SET USEFILE=%%A
-python rgb_converter.py %%A r g b
+python rgb_converter.py %%A r
 )
 pause
--- drop_here_to_scale.bat
+++ drop_here_to_scale.bat
@@ -1,6 +1,6 @@
 CD %~dp0
 FOR %%A IN (%*) DO (
 SET USEFILE=%%A
-python rgb_converter.py %%A vscale r g b
+python rgb_converter.py %%A vscale r
 )
 pause
